9926207cbcd25b07f7c6a8c74df69cfcb82fb7b7
Lewis Gosewinckel <gosewinckel.lewis@gmail.com>
2025-11-09 18:40:30 +1100

    testing on second computer

---
+++ microbenchmarks/disk_microbenchmarks.h
@@ -0,0 +1,11 @@
+# pragma once
+
+/**************************************************** 
+ * read_write_throughput -- read and write to disk 
+ *		speed
+ *
+ * Params: None
+ *
+ * Return: GB/s
+ ****************************************************/ 
+double disk_write_speed();
